ef476eb273b9775d5d88c5279c03d69eb003fd66
Amirreza_Moshtaghi <eng.masonbrown2.k@gmail.com>
2026-07-25 23:50:34 +0330

    auth: csrf token added to the form & request atribute used & redirect and reverse method is understood

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 # Create your views here.
 def contact_us(request):
+    if request.method == 'POST':
+        print(request.POST)
+        print(request.POST['email'])
+        return redirect(reverse('home'))
     return render(request, 'contact_module/contact_us.html',{})
--- product/admin.py
+++ product/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product, ProductsCategory, ProductTag,ProductBrand
 class ProductAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('title',)}
+    prepopulated_fields = {'slug':('titel',)}
     list_display = ['titel', 'price', 'is_active']
     list_filter = ['titel', 'price', 'category', 'is_active']
     list_editable = ['price', 'is_active']
