0d19595364712b9dc5aebbed126d8198ddfacb52
Amirreza_Moshtaghi <eng.masonbrown2.k@gmail.com>
2026-07-27 21:57:01 +0330

    forms: contact-forms added to html file with new attrs

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -5,9 +5,30 @@ from pkg_resources import require
 class ContactForm(forms.Form):
     name = forms.CharField(label='نام',
                            max_length=300,
-                           error_messages={'required': 'نام خود را وارد کنید'}
+                           error_messages={'required': 'نام خود را وارد کنید',},
+                           widget=forms.TextInput(
+                               attrs={
+                                   'class': 'form-control mt-1 mt-1',
+                                   'placeholder':'نام',
+                               }
                            )
-    email = forms.EmailField(label='ایمیل', widget=forms.EmailInput)
-    subject = forms.CharField(label='عنوان', max_length=300)
-    text = forms.CharField(label='متن', widget=forms.Textarea)
+                           )
+    email = forms.EmailField(label='ایمیل', widget=forms.EmailInput(
+        attrs={
+            'class': 'form-control mt-1',
+            'placeholder':'ایمیل'
+        }
+    ))
+    subject = forms.CharField(label='عنوان', max_length=300, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control mt-1',
+            'placeholder': 'عنوان'
+        }
+    ))
+    text = forms.CharField(label='متن', widget=forms.Textarea(
+        attrs={
+            'class': 'form-control mt-1',
+            'placeholder': 'متن'
+        }
+    ))
 
