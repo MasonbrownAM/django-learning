5a071aefc520ebf208005a9f4af01faabb0cf0ab
Amirreza_Moshtaghi <eng.masonbrown2.k@gmail.com>
2026-08-05 10:09:02 +0330

    UploadFile:upload the file with CBV

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import FormView, CreateView
+from django.views import View
 from .forms import ContactModelForm
 from .models import ContactUs
 
 
 # Create your views here.
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, 'contact_module/create_profile.html',)
+    def post(self, request):
+        print(request.POST)
+        return redirect(reverse('create_profile'))
+
+
 class ContactForm(CreateView):
     model = ContactUs
     form_class = ContactModelForm
